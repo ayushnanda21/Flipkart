221c4e41e45262d13e9115ed79dd3459c617e954
Ayush Nanda <ananda_be18@thapar.edu>
2022-03-03 15:13:18 +0530

    cart routes established

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const jwt = require("jsonwebtoken");
 const userRoute  = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute  = require("./routes/order");
 
 const app  = express();
 
@@ -29,7 +31,8 @@ app.use(express.json());
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
 app.use("/api/products", productRoute);
-
+app.use("/api/cart",cartRoute);
+app.use("/api/orders",orderRoute);
 
 
 //server running
--- routes/cart.js
+++ routes/cart.js
@@ -1,8 +1,82 @@
-const express = require("express");
-const router  = express.Router();
+//acquiring model
+const Cart = require("../models/Cart");
+//acquiring router
+const router  = require("express").Router();
+
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+//create 
+
+router.post("/", verifyToken, async(req,res) => {
+
+    const newCart = new Cart(req.body);
+
+    try{
+            const savedCart = await newCart.save();
+            res.status(200).json(savedCart);
+
+    } catch(err){
+        res.status(500).json(err);
+    }
+
+});
+
+//UPDATE
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+ 
+
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart has been cleared...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
+//GET user CART
+router.get("/find/:userId" ,verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const cart = await Cart.findOne({userId: req.params.userId });
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+// GET ALL CARTS OF ALL USER
+
+router.get("/", verifyTokenAndAdmin, async(req,res) => {
+
+    try{
+            const carts = await Cart.find()
+            res.status(200).json(carts);
+    } catch(err){
+        res.status(500).json(err);
+    }
+
+});
 
 //exporting
 module.exports = router
